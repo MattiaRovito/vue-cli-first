2fee3ecb0cdabafc8a27ad5a90bbca0886ddc7a7
MattiaR <rovitomattia91@gmail.com>
2021-07-05 23:16:46 +0200

    add esercizio+bonus

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="hero" alt="Vue logo" src="./assets/vue.png">
     <primoEsempio/>
   </div>
 </template>
@@ -17,12 +17,23 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
+#app 
+{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.title
+{
+  font-size: 30px;
+  color: blue;
+  background-color: yellow;
+}
+.hero
+{
+  width: 30%;
 }
 </style>
